a8a47846685ba5c2db36dae65a652fcf434aa4aa
yangsl17 <yangsl_thu@126.com>
2019-07-17 16:46:27 +0800

    solve conflict

---
+++ assets/scripts/logo.js
@@ -0,0 +1,32 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad : function() {
+        //获取本节点上的Animation组件
+        var animationComponent = this.getComponent(cc.Animation)
+        //执行play操作
+        var state = animationComponent.play('logo')
+        state.repeatCount = Infinity;
+    },
+    // update (dt) {},
+});
---
+++ assets/scripts/startButton.js
@@ -0,0 +1,15 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    toMenu : function() {
+        cc.director.loadScene("choiceMenu")
+    }
+
+    // update (dt) {},
+});
